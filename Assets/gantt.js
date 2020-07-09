2930304694cbf1b74026385e6da00a3934ccade8
BlueTeck <tili2@gmx.de>
2020-07-09 17:22:43 +0200

    [FEATURE] change scale

--- Assets/gantt.js
+++ Assets/gantt.js
@@ -1,6 +1,35 @@
 KB.on('dom.ready', function() {
     if (KB.exists('#gantt-chart')) {
+
+        console.log("load gantt chart");
+
         var tasks = jQuery("#gantt-chart").data("records");
-        var gantt = new Gantt("#gantt-chart", tasks);
+        var gantt = new Gantt("#gantt-chart", tasks, {
+            view_modes: ['Day', 'Week', 'Month'],
+            view_mode: 'Week',
+            date_format: 'YYYY-MM-DD',
+            custom_popup_html: null, //TODO
+            language: jQuery("html").attr('lang'),
+            on_click: function(task) {
+                console.log(task);
+            }
+        });
+
+        console.log("gantt chart loaded");
+
+        KB.onClick('#gantt-mode-day', function(e) {
+            console.log("mode day clicked");
+            gantt.change_view_mode('Day');
+        });
+
+        KB.onClick('#gantt-mode-week', function(e) {
+            console.log("mode week clicked");
+            gantt.change_view_mode('Week');
+        });
+
+        KB.onClick('#gantt-mode-month', function(e) {
+            console.log("mode month clicked");
+            gantt.change_view_mode('Month');
+        });
     }
 });
--- Template/task_gantt/show.php
+++ Template/task_gantt/show.php
@@ -1,7 +1,21 @@
 <section id="main">
     <?php echo $this->projectHeader->render($project, 'TaskGanttController', 'show', false, 'Gantt'); ?>
+    <div class="table-list-header">
+        <?php echo t('Gantt settings:'); ?>     
+        <ul class="views">
+            <li id="gantt-mode-day">
+                <?php echo t('Day'); ?>
+            </li>
+            <li id="gantt-mode-week">
+                <?php echo t('Week'); ?>
+            </li>
+            <li id="gantt-mode-month">
+                <?php echo t('Month'); ?>
+            </li>
+        </ul>
+    </div>
     <?php if (!empty($tasks)) { ?>
-        <div
+        <div        
             id="gantt-chart"
             data-records='<?php echo json_encode($tasks, JSON_HEX_APOS); ?>'
         ></div>        
