409022292709bcce2f9b68c6eea0621be9d30964
SARKISSIAN Ugo <ugo.sarkissian@enedis.fr>
2021-02-05 16:37:13 +0100

    update fr_FR translation

--- Locale/fr_FR/translations.php
+++ Locale/fr_FR/translations.php
@@ -3,4 +3,8 @@
 return [
     'Hide first Column' => 'Cacher la première colonne',
     'Progress' => 'Avancement',
+    'Gantt settings:' => 'Paramètres du Gantt :',
+    'Day' => 'Jour',
+    'Week' => 'Semaine',
+    'Month' => 'Mois',
 ];
