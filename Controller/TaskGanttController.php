772af114ad68ce41c6be323808f51bfd79f918a2
BlueTeck <tili2@gmx.de>
2021-02-23 19:19:16 +0100

    [INTERNAL] load tasks async

--- Controller/TaskGanttController.php
+++ Controller/TaskGanttController.php
@@ -4,7 +4,6 @@ namespace Kanboard\Plugin\Gantt\Controller;
 
 use Kanboard\Controller\BaseController;
 use Kanboard\Filter\TaskProjectFilter;
-use Kanboard\Model\TaskModel;
 
 class TaskGanttController extends BaseController
 {
@@ -15,19 +14,48 @@ class TaskGanttController extends BaseController
     {
         $project = $this->getProject();
         $search = $this->helper->projectHeader->getSearchQuery($project);
-        $filter = $this->taskLexer->build($search)->withFilter(new TaskProjectFilter($project['id']));
+        $filter = $this->taskLexer
+            ->build($search)
+            ->withFilter(new TaskProjectFilter($project["id"]));
 
-        $this->response->html($this->helper->layout->app('Gantt:task_gantt/show', [
-            'project' => $project,
-            'title' => $project['name'],
-            'description' => $this->helper->projectHeader->getDescription($project),
-            'tasks' => $filter->format($this->taskGanttFormatter),
-            'firstColumnId' => $this->columnModel->getFirstColumnId($project['id']),
-        ]));
+        $this->response->html(
+            $this->helper->layout->app("Gantt:task_gantt/show", [
+                "project" => $project,
+                "title" => $project["name"],
+                "description" => $this->helper->projectHeader->getDescription(
+                    $project
+                ),
+                "data_load_url" => $this->helper->url->href(
+                    "TaskGanttController",
+                    "data",
+                    [
+                        "project_id" => $project["id"],
+                        "search" => $search,
+                        "plugin" => "Gantt",
+                    ]
+                ),
+                "firstColumnId" => $this->columnModel->getFirstColumnId(
+                    $project["id"]
+                ),
+            ])
+        );
+    }
+
+    public function data()
+    {
+        $project = $this->getProject();
+        $search = $this->helper->projectHeader->getSearchQuery($project);
+        $filter = $this->taskLexer
+            ->build($search)
+            ->withFilter(new TaskProjectFilter($project["id"]));
+
+        $this->response->json([
+            "tasks" => $filter->format($this->taskGanttFormatter),
+        ]);
     }
 
     /**
-     * Save new task start date and due date
+     * Save new task start date and due date.
      */
     public function save()
     {
@@ -35,26 +63,29 @@ class TaskGanttController extends BaseController
         $changes = $this->request->getJson();
         $values = [];
 
-        if (!empty($changes['start'])) {
-            $values['date_started'] = strtotime($changes['start']);
+        if (!empty($changes["start"])) {
+            $values["date_started"] = strtotime($changes["start"]);
         }
 
-        if (!empty($changes['end'])) {
-            $values['date_due'] = strtotime($changes['end']);
+        if (!empty($changes["end"])) {
+            $values["date_due"] = strtotime($changes["end"]);
         }
 
         if (!empty($values)) {
-            $values['id'] = $changes['id'];
+            $values["id"] = $changes["id"];
             print_r($values);
             $result = $this->taskModificationModel->update($values);
 
             if (!$result) {
-                $this->response->json(array('message' => 'Unable to save task'), 400);
+                $this->response->json(
+                    ["message" => "Unable to save task"],
+                    400
+                );
             } else {
-                $this->response->json(array('message' => 'OK'), 201);
+                $this->response->json(["message" => "OK"], 201);
             }
         } else {
-            $this->response->json(array('message' => 'Ignored'), 200);
+            $this->response->json(["message" => "Ignored"], 200);
         }
     }
 }
--- Template/task_gantt/show.php
+++ Template/task_gantt/show.php
@@ -1,34 +1,46 @@
 <section id="main">
-    <?php echo $this->projectHeader->render($project, 'TaskGanttController', 'show', false, 'Gantt'); ?>
+    <?php echo $this->projectHeader->render(
+        $project,
+        "TaskGanttController",
+        "show",
+        false,
+        "Gantt"
+    ); ?>
     <div class="table-list-header">
-        <?php echo t('Gantt settings:'); ?>
+        <?php echo t("Gantt settings:"); ?>
         <ul class="gantt views">
             <li class="gantt-change-mode" data-mode-view="Day">
-                <?php echo t('Day'); ?>
+                <?php echo t("Day"); ?>
             </li>
             <li class="gantt-change-mode" data-mode-view="Week">
-                <?php echo t('Week'); ?>
+                <?php echo t("Week"); ?>
             </li>
             <li class="gantt-change-mode" data-mode-view="Month">
-                <?php echo t('Month'); ?>
+                <?php echo t("Month"); ?>
             </li>
             <li class="gantt-change-mode" data-mode-view="Year">
-                <?php echo t('Year'); ?>
+                <?php echo t("Year"); ?>
             </li>
             <li>
-                <label id="hide-first-column-label"><input type="checkbox" value="<?= $firstColumnId ?>" id="hide-first-column">&nbsp;<?= t('Hide first Column') ?></label>
+                <label id="hide-first-column-label"><input type="checkbox" value="<?= $firstColumnId ?>" id="hide-first-column">&nbsp;<?= t(
+    "Hide first Column"
+) ?></label>
             </li>
         </ul>
-    </div>
-    <?php if (!empty($tasks)) { ?>
+    </div>    
         <div id="gantt-chart"
-             data-save-url="<?php echo $this->url->href('TaskGanttController', 'save', ['project_id' => $project['id'], 'plugin' => 'Gantt']); ?>" 
-             data-records='<?php echo json_encode($tasks); ?>'
-        ></div>
-        <div id="gantt-popup-template" class="hide">
-            <?= $this->render('Gantt:task_gantt/popup', array('project' => $project, 'tasks' => $tasks)) ?>
+             data-save-url="<?php echo $this->url->href(
+                 "TaskGanttController",
+                 "save",
+                 ["project_id" => $project["id"], "plugin" => "Gantt"]
+             ); ?>" 
+             data-load-url="<?php echo $data_load_url; ?>" 
+        >
+        <i class="fa fa-spinner fa-2x fa-pulse"></i> Loading...
         </div>
-    <?php } else { ?>
-        <p class="alert"><?php echo t('There is no task in your project.'); ?></p>
-    <?php } ?>
+        <div id="gantt-popup-template" class="hide">
+            <?= $this->render("Gantt:task_gantt/popup", [
+                "project" => $project,
+            ]) ?>
+        </div>    
 </section>
